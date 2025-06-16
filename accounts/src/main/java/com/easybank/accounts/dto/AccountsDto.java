6bdf7d77529640a464591be2624786c88e06c55e
peter2s <petersalahsamir@gmail.com>
2025-06-16 21:36:51 +0300

    add global error handler and vaildtion for dtos

--- accounts/src/main/java/com/easybank/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/easybank/accounts/dto/AccountsDto.java
@@ -1,13 +1,18 @@
 package com.easybank.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
-
+    @NotEmpty(message = "Account Number can not be Empty")
+    @Pattern(regexp = "(^$[0-9]){10}",message = "Account Number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account Type can not be Empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch Address can not be Empty")
     private String BranchAddress;
 }
--- accounts/src/main/java/com/easybank/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/easybank/accounts/dto/CustomerDto.java
@@ -1,12 +1,23 @@
 package com.easybank.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message = "Name can not be Empty")
+    @Size(min = 5 , max = 30,message = "the length of customer name should be between 5 and 30 charter" )
     private String name;
+
+    @NotEmpty(message = "Email can not be Empty")
+    @Email(message = "Email Address should be valid email")
     private String email;
+
+    @Pattern(regexp = "(^$[0-9]){11}", message = "mobile number should be valid mobile number ")
     private String mobileNumber;
     private AccountsDto accountsDto;
 }

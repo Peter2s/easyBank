af6289f540c5da95da74c2ae42181021e9e1215f
peter2s <petersalahsamir@gmail.com>
2025-07-09 18:44:17 +0300

    update endpoints

--- accounts/src/main/java/com/easybank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/easybank/accounts/controller/AccountsController.java
@@ -4,33 +4,41 @@ import com.easybank.accounts.dto.CustomerDto;
 import com.easybank.accounts.dto.ResponseDto;
 import com.easybank.accounts.service.IAccountsService;
 import com.easybank.accounts.constants.AccountsConstants;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/accounts" , produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Validated
 public class AccountsController {
     private IAccountsService accountsService;
 
-    @PostMapping(path = "/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+    @PostMapping(path = "/")
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto){
         accountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201,AccountsConstants.MESSAGE_201));
     }
 
-    @GetMapping("/get")
-    public ResponseEntity<CustomerDto> getAccountDetails(@RequestParam String mobileNumber){
+    @GetMapping("/")
+    public ResponseEntity<CustomerDto> getAccountDetails(
+            @RequestParam
+            @Pattern(regexp = "(^$[0-9]){11}",message = "Mobile number should be valid Number must be 11 digits")
+            String mobileNumber)
+    {
         CustomerDto customerDto =  accountsService.getAccount(mobileNumber);
         return  ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
     @PutMapping("/update")
-    public  ResponseEntity<ResponseDto> updateAccount(@RequestBody CustomerDto customerDto){
+    public  ResponseEntity<ResponseDto> updateAccount(@Valid @RequestBody CustomerDto customerDto){
         boolean isUpdated = accountsService.updateAccount(customerDto);
         if (isUpdated){
             return  ResponseEntity.status(HttpStatus.OK)
@@ -40,7 +48,10 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_500,AccountsConstants.MESSAGE_500));
     }
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam String mobileNumber){
+    public ResponseEntity<ResponseDto> deleteAccount(
+            @RequestParam
+            @Pattern(regexp = "(^$[0-9]){11}",message = "Mobile number should be valid Number must be 11 digits")
+            String mobileNumber){
         boolean isDeleted = accountsService.deleteAccount(mobileNumber);
         if (isDeleted){
             return ResponseEntity.status(HttpStatus.NO_CONTENT)

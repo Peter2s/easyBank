852122d68caab1dadde31e94a49b2e25e7f0fe90
peter2s <petersalahsamir@gmail.com>
2025-07-09 18:45:49 +0300

    handle method Argument not Valid

--- accounts/src/main/java/com/easybank/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/easybank/accounts/exception/GlobalExceptionHandler.java
@@ -2,16 +2,25 @@ package com.easybank.accounts.exception;
 
 import com.easybank.accounts.dto.ErrorResponseDto;
 import com.easybank.accounts.dto.ResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler  extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException
@@ -24,6 +33,7 @@ public class GlobalExceptionHandler {
         );
         return  new ResponseEntity<>(errorResponseDto,HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException
             (CustomerAlreadyExistsException exception, WebRequest webRequest){
@@ -37,7 +47,6 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-
     public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException
             (ResourceNotFoundException exception, WebRequest webRequest){
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(
@@ -47,5 +56,22 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return  new ResponseEntity<>(errorResponseDto,HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String,String> vaildtionErros = new HashMap<>();
+        List<ObjectError> vaildtionErrorList = ex.getBindingResult().getAllErrors();
+        vaildtionErrorList.forEach((error)->{
+            String fieldName = ((FieldError)error).getField();
+            String vaildtionMsg  = error.getDefaultMessage();
+            vaildtionErros.put(fieldName,vaildtionMsg);
+
+        });
+
+        return new ResponseEntity<>(vaildtionErros,HttpStatus.BAD_REQUEST);
     }
 }
